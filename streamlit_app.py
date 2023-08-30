72f1bfeeb6ff37ae0b95b0753dd6acf50f9fce05
mustufabaig <mustufa.baig@gmail.com>
2023-08-30 17:49:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ database = st.secrets["database"]
 schema = st.secrets["schema"]
 
 snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
-db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=10, include_tables=['peer_benchmark'])
+db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=10, include_tables=['merchant','my_me_merchant_benchmark','my_peer_merchant_benchmark','my_region_merchant_benchmark'])
 toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
 
 #https://python.langchain.com/docs/integrations/toolkits/sql_database#using-zero_shot_react_description
