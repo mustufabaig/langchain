7cd14806291733428f43076b1fab6f8877c2e88a
mustufabaig <mustufa.baig@gmail.com>
2023-08-30 10:46:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,31 @@
+import streamlit as st
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+from langchain.llms.openai import OpenAI
+from langchain.agents import AgentExecutor
+from langchain.agents.agent_types import AgentType
+from langchain.chat_models import ChatOpenAI
 
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+warehouse = st.secrets["warehouse"]
+role = st.secrets["role"]
+snowflake_account = st.secrets["account"]
+database = st.secrets["database"]
+schema = st.secrets["schema"]
+
+snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=10, include_tables=['peer_benchmark'])
+toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
+
+#https://python.langchain.com/docs/integrations/toolkits/sql_database#using-zero_shot_react_description
+agent_executor = create_sql_agent(
+    llm=OpenAI(temperature=0),
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
+
+st.write("configuration done")
