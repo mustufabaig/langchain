54cfd82fd2bcbf3b73a22e394cfacb7c7a1400f0
mustufabaig <mustufa.baig@gmail.com>
2023-08-30 21:15:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,17 +16,28 @@ snowflake_account = st.secrets["account"]
 database = st.secrets["database"]
 schema = st.secrets["schema"]
 
-snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
-db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=5, include_tables=['merchant','my_me_merchant_benchmark','my_peer_merchant_benchmark','my_region_merchant_benchmark'])
-toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
+def prepare_agent():
+    snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+    db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=5, include_tables=['merchant','my_me_merchant_benchmark','my_peer_merchant_benchmark','my_region_merchant_benchmark'])
+    toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
+    
+    #https://python.langchain.com/docs/integrations/toolkits/sql_database#using-zero_shot_react_description
+    agent_executor = create_sql_agent(
+        llm=OpenAI(temperature=0),
+        toolkit=toolkit,
+        verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    )
+    session["agent_executor"] = agent_executor
+    return agent_executor
 
-#https://python.langchain.com/docs/integrations/toolkits/sql_database#using-zero_shot_react_description
-agent_executor = create_sql_agent(
-    llm=OpenAI(temperature=0),
-    toolkit=toolkit,
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-)
+agent = session["agent_executor"]
+if not agent_executor:
+    agent = prepare_agent()
 
-st.write("configuration done")
-st.write(agent_executor.run("how do I compare against my peers in cross border fraud performance?"))
+prompt = st.chat_input("How can I help you?")
+if prompt:
+    with st.spinner('Looking for answers...'):
+        with st.chat_message("assistant"):
+            st.write("here is what I have found...")
+            st.info(agent_executor.run("how do I compare against my peers in cross border fraud performance?");
